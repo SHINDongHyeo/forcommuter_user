3a2dcca6178a0eaec9d2608c89df456e2df56317
Donghyeok Shin <smdh1010@gmail.com>
2024-10-02 21:01:10 +0900

    feature: complete

--- src/user.controller.ts
+++ src/user.controller.ts
@@ -5,40 +5,40 @@ import { updateNickRes, validateNickRes } from './user.interface';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) { }
+	constructor(private readonly userService: UserService) { }
 
-  // 내 정보 조회
-  @Get(':socialId')
-  async getUser(
-    @Param('socialId') socialId: string,
-  ): Promise<UserDto> {
-    return this.userService.getUser(socialId);
-  }
+	// 내 정보 조회
+	@Get(':socialId')
+	async getUser(
+		@Param('socialId') socialId: string,
+	): Promise<UserDto> {
+		return this.userService.getUser(socialId);
+	}
 
-  // 닉네임 중복 검사
-  @Post('nick/validate')
-  async validateNick(
-    @Body('nick') nick: string,
-  ): Promise<validateNickRes> {
-    return this.userService.validateNick(nick);
-  }
+	// 닉네임 중복 검사
+	@Post('nick/validate')
+	async validateNick(
+		@Body('nick') nick: string,
+	): Promise<validateNickRes> {
+		return this.userService.validateNick(nick);
+	}
 
-  // 닉네임 수정
-  @Patch('nick/:socialId')
-  async updateNick(
-    @Param('socialId') socialId: string,
-    @Body('newNick') newNick: string,
-  ): Promise<updateNickRes> {
-    return this.userService.updateNick(socialId, newNick);
-  }
+	// 닉네임 수정
+	@Patch('nick/:socialId')
+	async updateNick(
+		@Param('socialId') socialId: string,
+		@Body('newNick') newNick: string,
+	): Promise<updateNickRes> {
+		return this.userService.updateNick(socialId, newNick);
+	}
 
-  // 탈퇴
-  @Delete(':socialId')
-  async deleteUser(
-    @Param('socialId') socialId: string,
-    @Body('deleteReasonIds') deleteReasonIds: Array<number>,
-    @Body('deleteEtc') deleteEtc: string | null,
-  ): Promise<void> {
-    return this.userService.deleteUser(socialId, deleteReasonIds, deleteEtc);
-  }
+	// 탈퇴
+	@Delete(':socialId')
+	async deleteUser(
+		@Param('socialId') socialId: string,
+		@Body('deleteReasonIds') deleteReasonIds: Array<number>,
+		@Body('deleteEtc') deleteEtc: string | null,
+	): Promise<void> {
+		return this.userService.deleteUser(socialId, deleteReasonIds, deleteEtc);
+	}
 }
